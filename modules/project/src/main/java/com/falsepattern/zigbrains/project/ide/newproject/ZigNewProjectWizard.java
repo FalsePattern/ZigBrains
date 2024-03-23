7cdbc443f18da4e8481cf794c8341e4c79de5b36
FalsePattern <me@falsepattern.com>
2024-03-23 22:51:29 +0100

    backport: 13.1.1
    ci: 13.1.1
    
    fix: write access error on project creation
    
    fix: find usages regression due to action ID change
    
    chore: Improved license wording
    
    (cherry picked from commit 61e866ecc8ef3d6ef9f8cc499b8ddc7e2f568c2b)

--- modules/project/src/main/java/com/falsepattern/zigbrains/project/ide/newproject/ZigNewProjectWizard.java
+++ modules/project/src/main/java/com/falsepattern/zigbrains/project/ide/newproject/ZigNewProjectWizard.java
@@ -16,6 +16,7 @@
 
 package com.falsepattern.zigbrains.project.ide.newproject;
 
+import com.falsepattern.zigbrains.common.util.ApplicationUtil;
 import com.falsepattern.zigbrains.project.ide.util.projectwizard.ZigModuleBuilder;
 import com.falsepattern.zigbrains.project.platform.ZigProjectGeneratorPeer;
 import com.intellij.ide.wizard.AbstractNewProjectWizardStep;
@@ -88,7 +89,7 @@ public class ZigNewProjectWizard implements LanguageNewProjectWizard {
                     return;
                 }
                 if (gitData.getGit()) {
-                    createGitIgnoreFile(getContext().getProjectDirectory(), module);
+                    ApplicationUtil.writeAction(() -> createGitIgnoreFile(getContext().getProjectDirectory(), module));
                 }
             });
         }
