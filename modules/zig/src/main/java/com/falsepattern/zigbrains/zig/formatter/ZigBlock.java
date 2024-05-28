fca865992094d627307f50ed624c38eb967e6dfa
FalsePattern <me@falsepattern.com>
2024-05-28 02:45:48 +0200

    backport: 14.4.0

--- modules/zig/src/main/java/com/falsepattern/zigbrains/zig/formatter/ZigBlock.java
+++ modules/zig/src/main/java/com/falsepattern/zigbrains/zig/formatter/ZigBlock.java
@@ -39,11 +39,18 @@ import static com.falsepattern.zigbrains.zig.psi.ZigTypes.CONTAINER_DECL_TYPE;
 import static com.falsepattern.zigbrains.zig.psi.ZigTypes.CONTAINER_DOC_COMMENT;
 import static com.falsepattern.zigbrains.zig.psi.ZigTypes.EXPR_LIST;
 import static com.falsepattern.zigbrains.zig.psi.ZigTypes.FN_CALL_ARGUMENTS;
+import static com.falsepattern.zigbrains.zig.psi.ZigTypes.IF_EXPR;
+import static com.falsepattern.zigbrains.zig.psi.ZigTypes.IF_PREFIX;
+import static com.falsepattern.zigbrains.zig.psi.ZigTypes.IF_STATEMENT;
 import static com.falsepattern.zigbrains.zig.psi.ZigTypes.INIT_LIST;
+import static com.falsepattern.zigbrains.zig.psi.ZigTypes.KEYWORD_ELSE;
 import static com.falsepattern.zigbrains.zig.psi.ZigTypes.LBRACE;
 import static com.falsepattern.zigbrains.zig.psi.ZigTypes.LPAREN;
+import static com.falsepattern.zigbrains.zig.psi.ZigTypes.PARAM_DECL;
+import static com.falsepattern.zigbrains.zig.psi.ZigTypes.PARAM_DECL_LIST;
 import static com.falsepattern.zigbrains.zig.psi.ZigTypes.RBRACE;
 import static com.falsepattern.zigbrains.zig.psi.ZigTypes.RPAREN;
+import static com.falsepattern.zigbrains.zig.psi.ZigTypes.SWITCH_PRONG_LIST;
 
 public class ZigBlock extends AbstractBlock {
 
@@ -95,13 +102,37 @@ public class ZigBlock extends AbstractBlock {
     }
 
     private static Indent getIndentBasedOnParentType(IElementType parentElementType, IElementType childElementType) {
-        if ((parentElementType == BLOCK && childElementType != LBRACE && childElementType != RBRACE) ||
-            (parentElementType == INIT_LIST && childElementType != LBRACE && childElementType != RBRACE) ||
-            parentElementType == EXPR_LIST ||
-            (parentElementType == FN_CALL_ARGUMENTS && childElementType != LPAREN && childElementType != RPAREN) ||
-            (parentElementType == CONTAINER_DECL_AUTO && childElementType != CONTAINER_DECL_TYPE && childElementType != LBRACE && childElementType != CONTAINER_DOC_COMMENT && childElementType != RBRACE)) {
+        //Statement blocks
+        if (parentElementType == BLOCK && childElementType != LBRACE && childElementType != RBRACE)
             return Indent.getNormalIndent();
-        }
+
+        //Struct/tuple initializers
+        if (parentElementType == INIT_LIST && childElementType != LBRACE && childElementType != RBRACE)
+            return Indent.getNormalIndent();
+
+        //Comma expressions
+        if (parentElementType == EXPR_LIST)
+            return Indent.getNormalIndent();
+
+        //Function call args
+        if (parentElementType == FN_CALL_ARGUMENTS && childElementType != LPAREN && childElementType != RPAREN)
+            return Indent.getNormalIndent();
+
+        //Function declaration parameters
+        if (parentElementType == PARAM_DECL_LIST)
+            return Indent.getNormalIndent();
+
+        //Switch prongs
+        if (parentElementType == SWITCH_PRONG_LIST)
+            return Indent.getNormalIndent();
+
+        //If expressions/statements
+        if ((parentElementType == IF_EXPR || parentElementType == IF_STATEMENT) && childElementType != KEYWORD_ELSE && childElementType != IF_PREFIX)
+            return Indent.getNormalIndent();
+
+        if (parentElementType == CONTAINER_DECL_AUTO && childElementType != CONTAINER_DECL_TYPE && childElementType != LBRACE && childElementType != CONTAINER_DOC_COMMENT && childElementType != RBRACE)
+            return Indent.getNormalIndent();
+
         return Indent.getNoneIndent();
     }
 }
