5bfda2f67a311024e6eee9c5037c6a810b82ab19
FalsePattern <me@falsepattern.com>
2024-10-03 14:53:49 +0200

    backport: 17.3.1 dev 1

--- modules/zig/src/main/java/com/falsepattern/zigbrains/zig/lsp/ZLSLanguageServerFactory.java
+++ modules/zig/src/main/java/com/falsepattern/zigbrains/zig/lsp/ZLSLanguageServerFactory.java
@@ -1,9 +1,12 @@
 package com.falsepattern.zigbrains.zig.lsp;
 
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiFile;
 import com.redhat.devtools.lsp4ij.LanguageServerEnablementSupport;
 import com.redhat.devtools.lsp4ij.LanguageServerFactory;
 import com.redhat.devtools.lsp4ij.client.LanguageClientImpl;
+import com.redhat.devtools.lsp4ij.client.features.LSPClientFeatures;
+import com.redhat.devtools.lsp4ij.client.features.LSPFormattingFeature;
 import com.redhat.devtools.lsp4ij.server.StreamConnectionProvider;
 import org.jetbrains.annotations.NotNull;
 
@@ -12,6 +15,18 @@ public class ZLSLanguageServerFactory implements LanguageServerFactory, Language
     @Override
     public @NotNull StreamConnectionProvider createConnectionProvider(@NotNull Project project) {
         return new ZLSStreamConnectionProvider(project);
+    }
+
+    @SuppressWarnings("UnstableApiUsage")
+    @Override
+    public @NotNull LSPClientFeatures createClientFeatures() {
+        return new LSPClientFeatures()
+                .setFormattingFeature(new LSPFormattingFeature() {
+                    @Override
+                    protected boolean isExistingFormatterOverrideable(@NotNull PsiFile file) {
+                        return true;
+                    }
+                });
     }
 
     @Override
