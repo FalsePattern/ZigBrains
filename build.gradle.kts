18747eca39220dc3178c7d21c682fc34002e2c38
FalsePattern <me@falsepattern.com>
2024-10-03 14:53:49 +0200

    ci: fix error on dist

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,6 +56,9 @@ tasks {
     wrapper {
         gradleVersion = properties("gradleVersion").get()
     }
+    prepareJarSearchableOptions {
+        enabled = false
+    }
 }
 
 fun pluginVersion(): Provider<String> {
