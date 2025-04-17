8209e688fcbb968d479c756e1fd5eeaf7b38e18b
FalsePattern <me@falsepattern.com>
2025-04-17 13:45:42 +0200

    backport: 25.1.0

--- lsp/build.gradle.kts
+++ lsp/build.gradle.kts
@@ -8,12 +8,15 @@ val lsp4ijVersion: String by project
 val lsp4jVersion: String by project
 val ideaCommunityVersion: String by project
 val useInstaller = property("useInstaller").toString().toBoolean()
+val serializationVersion: String by project
 
 dependencies {
     intellijPlatform {
         create(IntelliJPlatformType.IntellijIdeaCommunity, ideaCommunityVersion, useInstaller = useInstaller)
     }
-    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-core:1.7.3")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-core-jvm:$serializationVersion") {
+        isTransitive = false
+    }
     compileOnly("com.redhat.devtools.intellij:lsp4ij:$lsp4ijVersion")
     compileOnly("org.eclipse.lsp4j:org.eclipse.lsp4j:$lsp4jVersion")
     implementation(project(":core")) {
