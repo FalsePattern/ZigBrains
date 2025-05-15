f02e3750a9b7384586002e344b7333f4175706a2
FalsePattern <me@falsepattern.com>
2025-05-15 15:41:59 +0200

    backport: 25.5.0

--- lsp/build.gradle.kts
+++ lsp/build.gradle.kts
@@ -6,18 +6,25 @@ plugins {
 
 val lsp4ijVersion: String by project
 val lsp4jVersion: String by project
+val lsp4ijNightly = property("lsp4ijNightly").toString().toBoolean()
 val ideaCommunityVersion: String by project
 val useInstaller = property("useInstaller").toString().toBoolean()
 val serializationVersion: String by project
+val lsp4ijPluginString = "com.redhat.devtools.lsp4ij:$lsp4ijVersion${if (lsp4ijNightly) "@nightly" else ""}"
 
 dependencies {
     intellijPlatform {
         create(IntelliJPlatformType.IntellijIdeaCommunity, ideaCommunityVersion, useInstaller = useInstaller)
+        if (lsp4ijNightly) {
+            plugin(lsp4ijPluginString)
+        }
     }
     compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-core-jvm:$serializationVersion") {
         isTransitive = false
     }
-    compileOnly("com.redhat.devtools.intellij:lsp4ij:$lsp4ijVersion")
+    if (!lsp4ijNightly) {
+        compileOnly("com.redhat.devtools.intellij:lsp4ij:$lsp4ijVersion")
+    }
     compileOnly("org.eclipse.lsp4j:org.eclipse.lsp4j:$lsp4jVersion")
     implementation(project(":core")) {
         isTransitive = false
