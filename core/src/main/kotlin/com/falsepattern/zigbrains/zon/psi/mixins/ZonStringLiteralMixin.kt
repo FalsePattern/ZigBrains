238a2ccba27013b705f40a51681938e5192c6072
FalsePattern <me@falsepattern.com>
2025-07-18 22:24:28 +0200

    backport: 26.3.0

--- core/src/main/kotlin/com/falsepattern/zigbrains/zon/psi/mixins/ZonStringLiteralMixin.kt
+++ core/src/main/kotlin/com/falsepattern/zigbrains/zon/psi/mixins/ZonStringLiteralMixin.kt
@@ -1,28 +1,27 @@
 /*
- * This file is part of ZigBrains.
- *
+ * ZigBrains
  * Copyright (C) 2023-2025 FalsePattern
  * All Rights Reserved
  *
  * The above copyright notice and this permission notice shall be included
  * in all copies or substantial portions of the Software.
  *
- * ZigBrains is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, only version 3 of the License.
  *
- * ZigBrains is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with ZigBrains. If not, see <https://www.gnu.org/licenses/>.
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
-package com.falsepattern.zigbrains.zig.psi.mixins
+package com.falsepattern.zigbrains.zon.psi.mixins
 
 import com.falsepattern.zigbrains.shared.psi.StringLiteral
 
-interface ZigStringLiteralMixin: StringLiteral {
+interface ZonStringLiteralMixin: StringLiteral {
 }
