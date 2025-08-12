eb406aec22208c3d4e8716ee6741f430eef80c16
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2025-08-12 14:22:21 +0200

    feat: Rework `zls-bad-config` in-editor banner to directly open the relevant settings

---
+++ lsp/src/main/kotlin/com/falsepattern/zigbrains/lsp/action/OpenZigToolchainConfigurableAction.kt
@@ -0,0 +1,33 @@
+/*
+ * ZigBrains
+ * Copyright (C) 2023-2025 FalsePattern
+ * All Rights Reserved
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, only version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.falsepattern.zigbrains.lsp.action
+
+import com.falsepattern.zigbrains.project.settings.ZigConfigurable
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.options.ShowSettingsUtil
+
+class OpenZigToolchainConfigurableAction: AnAction() {
+	override fun actionPerformed(e: AnActionEvent) {
+		ShowSettingsUtil.getInstance().showSettingsDialog( e.project!!, ZigConfigurable::class.java )
+	}
+}
