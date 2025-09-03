bce2c409d2e5810df8f86998f6196295564596fc
ENDERZOMBI102 <enderzombi102.end@gmail.com>
2025-09-04 01:15:44 +0200

    feat: Add auto-completion to steps' textbox

--- core/src/main/kotlin/com/falsepattern/zigbrains/project/execution/build/ZigExecConfigBuild.kt
+++ core/src/main/kotlin/com/falsepattern/zigbrains/project/execution/build/ZigExecConfigBuild.kt
@@ -23,24 +23,34 @@
 package com.falsepattern.zigbrains.project.execution.build
 
 import com.falsepattern.zigbrains.ZigBrainsBundle
-import com.falsepattern.zigbrains.project.execution.base.ArgsConfigurable
-import com.falsepattern.zigbrains.project.execution.base.FilePathConfigurable
-import com.falsepattern.zigbrains.project.execution.base.ZigConfigurable
-import com.falsepattern.zigbrains.project.execution.base.ZigExecConfig
-import com.falsepattern.zigbrains.project.execution.base.ZigProfileState
+import com.falsepattern.zigbrains.project.buildscan.Serialization
+import com.falsepattern.zigbrains.project.buildscan.zigBuildScan
+import com.falsepattern.zigbrains.project.execution.base.*
 import com.falsepattern.zigbrains.shared.ZBFeatures
+import com.falsepattern.zigbrains.shared.cli.translateCommandline
+import com.falsepattern.zigbrains.shared.element.readString
+import com.falsepattern.zigbrains.shared.element.readStrings
+import com.falsepattern.zigbrains.shared.element.writeString
 import com.intellij.execution.ExecutionException
 import com.intellij.execution.Executor
 import com.intellij.execution.configurations.ConfigurationFactory
 import com.intellij.execution.runners.ExecutionEnvironment
+import com.intellij.icons.AllIcons
 import com.intellij.openapi.project.Project
+import com.intellij.ui.TextFieldWithAutoCompletion
+import com.intellij.ui.TextFieldWithAutoCompletionListProvider
+import com.intellij.ui.dsl.builder.AlignX
+import com.intellij.ui.dsl.builder.Panel
+import org.jdom.Element
+import org.jetbrains.annotations.Nls
+import javax.swing.Icon
 
 class ZigExecConfigBuild(project: Project, factory: ConfigurationFactory): ZigExecConfig<ZigExecConfigBuild>(project, factory, ZigBrainsBundle.message("exec.type.build.label")) {
-    var buildSteps = ArgsConfigurable("buildSteps", ZigBrainsBundle.message("exec.option.label.build.steps"))
+    var buildSteps = StepsConfigurable(project, "buildSteps", ZigBrainsBundle.message("exec.option.label.build.steps"))
         private set
     var extraArgs = ArgsConfigurable("compilerArgs", ZigBrainsBundle.message("exec.option.label.build.args"), true)
         private set
-    var debugBuildSteps = ArgsConfigurable("debugBuildSteps", ZigBrainsBundle.message("exec.option.label.build.steps-debug"))
+    var debugBuildSteps = StepsConfigurable(project, "debugBuildSteps", ZigBrainsBundle.message("exec.option.label.build.steps-debug"))
         private set
     var debugExtraArgs = ArgsConfigurable("debugCompilerArgs", ZigBrainsBundle.message("exec.option.label.build.args-debug"), true)
         private set
@@ -83,4 +93,83 @@ class ZigExecConfigBuild(project: Project, factory: ConfigurationFactory): ZigEx
     override fun getState(executor: Executor, environment: ExecutionEnvironment): ZigProfileState<ZigExecConfigBuild> {
         return ZigProfileStateBuild(environment, this)
     }
+}
+
+
+class StepsConfigurable(
+	@Transient private val project: Project,
+	@Transient private val serializedName: String,
+	@Transient @Nls private val guiName: String
+) : ZigConfigurable<StepsConfigurable>, Cloneable {
+	var args: String = ""
+
+	override fun readExternal(element: Element) {
+		args = element.readString(serializedName) ?: element.readStrings(serializedName)?.joinToString(separator = " ") { if (it.contains(' ')) "\"$it\"" else it } ?: ""
+	}
+
+	fun argsSplit(): List<String> {
+		return translateCommandline(args)
+	}
+
+	override fun writeExternal(element: Element) {
+		element.writeString(serializedName, args)
+	}
+
+	override fun createEditor(): ZigConfigurable.ZigConfigModule<StepsConfigurable> {
+		return StepsConfigModule(project, serializedName, guiName)
+	}
+
+	override fun clone(): StepsConfigurable {
+		return super<Cloneable>.clone() as StepsConfigurable
+	}
+
+	class StepsConfigModule(
+		project: Project,
+		private val serializedName: String,
+		@Nls private val guiName: String
+	) : ZigConfigurable.ZigConfigModule<StepsConfigurable> {
+		private val argsField = TextFieldWithAutoCompletion(project, StepsTextFieldCompletionProvider(project), true, "Steps...")
+
+		override fun tryMatch(cfg: ZigConfigurable<*>): StepsConfigurable? {
+			return if (cfg is StepsConfigurable && cfg.serializedName == serializedName) cfg else null
+		}
+
+		override fun apply(configurable: StepsConfigurable): Boolean {
+			configurable.args = argsField.text
+			return true
+		}
+
+		override fun reset(configurable: StepsConfigurable) {
+			argsField.text = configurable.args
+		}
+
+		override fun construct(p: Panel): Unit = with(p) {
+			row(guiName) {
+				cell(argsField).align(AlignX.FILL)
+			}
+		}
+
+		override fun dispose() {
+
+		}
+
+		class StepsTextFieldCompletionProvider(project: Project) : TextFieldWithAutoCompletionListProvider<Serialization.Step>(project.zigBuildScan.rootProject?.steps) {
+			override fun getLookupString(item: Serialization.Step): String =
+				item.name
+
+			override fun getIcon(item: Serialization.Step): Icon? {
+				return when(item.kind) {
+					"install" -> AllIcons.Actions.Install
+					"uninstall" -> AllIcons.Actions.Uninstall
+					else -> AllIcons.RunConfigurations.TestState.Run
+				}
+			}
+
+			override fun getTypeText(item: Serialization.Step): String? =
+				item.kind
+
+			override fun getTailText(item: Serialization.Step): String? =
+				"   ${item.description}"
+		}
+	}
 }
