74e3627205e8b2ef8d0e71ab8b5d9aac943f3650
FalsePattern <me@falsepattern.com>
2025-09-04 01:10:00 +0200

    backport: 28.0.0

--- core/src/main/kotlin/com/falsepattern/zigbrains/errorreporter/ErrorResponse.kt
+++ core/src/main/kotlin/com/falsepattern/zigbrains/errorreporter/ErrorResponse.kt
@@ -4,13 +4,14 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class ErrorResponse(
-    val url: String,
-    val linkText: String,
+    val url: String?,
+    val linkText: String?,
     val status: Status,
 ) {
     @Serializable
     enum class Status {
         New,
         Duplicate,
+        OldVersion,
     }
 }
